5f9f3cc93e878fac6873153ddf57f2301ab15802
(faizsltn) <(faizsulthondaudm@gmail.com)>
2026-04-14 11:27:26 +0700

    logika

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -3,3 +3,44 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
+namespace CRUDMahasiswaADO
+{
+    public partial class FormMahasiswa : Form
+    {
+        private readonly string connectionString = "Data Source=Faizsltn-27\\FAIZSLTN; Initial Catalog=DBAkademikADO; Integrated Security=True";
+        private SqlConnection conn;
+
+        public FormMahasiswa()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbJK.Items.Clear();
+            cmbJK.Items.Add("L");
+            cmbJK.Items.Add("P");
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void ConnectDatabase()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                MessageBox.Show("Koneksi berhasil");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal: " + ex.Message);
+            }
+        }
